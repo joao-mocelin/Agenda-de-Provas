db82f22bdbcbad8c1e2288df15526f0e02abbbb2
João Mocelin <joaomocedol@gmail.com>
2026-07-16 00:48:18 -0300

    Carga do arquivo ok

--- main.c
+++ main.c
@@ -1,32 +1,51 @@
 #include "agenda.h"
 
 int main(){
-    struct prova *provas;
+    struct prova *provas = NULL;
     int quant_provas = 0;
     FILE *arquivo;
     arquivo = fopen("agenda.txt","rt");
     if(arquivo == NULL){
-        printf("\nErro ao ler agenda.");
+        printf("\nErro ao ler agenda.\n");
         system("pause");
     }
     else{
-        fscanf(arquivo, "%d \n", &quant_provas);
-        if(quant_provas > 0){
-            struct prova *temp = malloc(sizeof(struct prova) * quant_provas);
+        if(fscanf(arquivo, "%d\n", &quant_provas) != 1){ //deve ler somente um valor
+            quant_provas = 0;
+            printf("\nErro: Agenda de provas corrompida.\n");
+            fclose(arquivo);
+            system("pause");
+        }
+        else if(quant_provas > 0){
+            provas = malloc(sizeof(struct prova) * quant_provas);
+            if(provas == NULL){
+                printf("\nErro: Falha na alocação de memória.\n");
+                fclose(arquivo);
+                system("pause");
+                return 1;
+            }
             for(int i = 0; i < quant_provas; i++){
-                fscanf(arquivo, "%d %d %d \n", temp[i].data.dia, temp[i].data.mes, temp[i].data.ano);
-                fscanf(arquivo, "%d %d \n", temp[i].horario_ini.hora, temp[i].horario_ini.minuto);
-                fscanf(arquivo, "%d %d \n", temp[i].horario_fim.hora, temp[i].horario_fim.minuto);
-                fscanf(arquivo, " %50[^\n]", temp[i].desc);
-                fscanf(arquivo, " %50[^\n]", temp[i].local);
+                fscanf(arquivo, "%d %d %d\n", &provas[i].data.dia, &provas[i].data.mes, &provas[i].data.ano);
+                fscanf(arquivo, "%d %d\n", &provas[i].horario_ini.hora, &provas[i].horario_ini.minuto);
+                fscanf(arquivo, "%d %d\n", &provas[i].horario_fim.hora, &provas[i].horario_fim.minuto);
+                fscanf(arquivo, "%50[^\n]\n", provas[i].desc);
+                fscanf(arquivo, "%50[^\n]\n", provas[i].local);
+            }
+            fclose(arquivo);
+            if(quant_provas == 1){
+                printf("\n1 Prova encontrada.\n");
+                system("pause");
+            }
+            else{
+                printf("\n%d Provas encontradas.\n",quant_provas);
+                system("pause");
             }
         }
-        else{
-            printf("\nAgenda de provas vazia.\nInicializando. . .");
+        else{ //quant_provas == 0
+            fclose(arquivo);
+            printf("\nAgenda de provas vazia.\nInicializando. . .\n");
             system("pause");
         }
     }
-
-
-    return 1;
+    return 0;
 }
