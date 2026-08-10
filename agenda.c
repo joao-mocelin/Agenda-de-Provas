c87efc069ef9e1d40cec2c9c64da54fe3b639123
João Mocelin <joaomocedol@gmail.com>
2026-08-10 17:11:56 -0300

    Melhoria na legibilidade da função check_data

--- agenda.c
+++ agenda.c
@@ -27,34 +27,19 @@ struct prova *cadastra_prova(struct prova *agenda, int quantidade_provas){
 }
 
 int check_data(struct data data_a_checar){ //return 1 == Invalid data; return 0 == valid data
-    int mes[7] = {1,3,5,7,8,10,12};
-    int eh_bissexto = 0;
+    int mes[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
     if(data_a_checar.ano < 1900 || data_a_checar.ano > 2100){
         printf("\nAno inválido!");
         return 1;
     }
     if((data_a_checar.ano % 4 == 0 && data_a_checar.ano % 100 != 0) || data_a_checar.ano % 400 == 0){
-        eh_bissexto = 1;
+        mes[1] = 29;
     }
     if(data_a_checar.mes < 1 || data_a_checar.mes > 12){
         printf("\nMês inválido!");
         return 1;
     }
-    int max_dias = 30;
-    if(data_a_checar.mes == 2){
-        if(eh_bissexto == 1){
-            max_dias = 29;
-        }
-        else{
-            max_dias = 28;
-        }
-    }
-    for (int i = 0; i < 7; i++)
-    {
-        if(data_a_checar.mes == mes[i]){
-            max_dias = 31;
-        }
-    }
+    int max_dias = mes[data_a_checar.mes - 1];
     if(data_a_checar.dia < 1 || data_a_checar.dia > max_dias){
         printf("\nDia inválido!");
         return 1;
